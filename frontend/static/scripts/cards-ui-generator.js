72575f338d56b30a030f0a9e12cda7433eb54bcd
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2023-04-28 13:31:37 -0700

    Playing cards able to be displayed

---
+++ frontend/static/scripts/cards-ui-generator.js
@@ -0,0 +1,36 @@
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(addCardElements);
+
+function addCardElements(card) {
+  const value = card.dataset.value;
+
+  const valueAsNumber = parseInt(value);
+  if (isNaN(valueAsNumber)) {
+    if (value === "A") card.append(createPip());
+    else {
+      card.append(createPip());
+    }
+  } else {
+    for (let i = 0; i < valueAsNumber; i++) card.append(createPip());
+  }
+
+  if (value !== "unknown") {
+    card.append(createCornerNumber("top", value));
+    card.append(createCornerNumber("bottom", value));
+  }
+}
+
+function createCornerNumber(position, value) {
+  const corner = document.createElement("div");
+  corner.textContent = value;
+  corner.classList.add("corner-number");
+  corner.classList.add(position);
+  return corner;
+}
+
+function createPip() {
+  const pip = document.createElement("div");
+  pip.classList.add("pip");
+  return pip;
+}

e563a0d8dbe5aa057da61d9cb73eb6c5fa07dc25
ThangTLuong <78049217+ThangTLuong@users.noreply.github.com>
2023-05-07 14:04:44 -0700

    Cards can now be dynamically generated with random values

--- backend/static/scripts/cards-ui-generator.js
+++ backend/static/scripts/cards-ui-generator.js
@@ -1,6 +1,60 @@
+const display = document.getElementsByClassName("hand");
 const cards = document.querySelectorAll(".card");
 
 cards.forEach(addCardElements);
+
+function addcards(count) {
+  const newHand = document.createElement("div");
+  newHand.classList.add("card");
+  newHand.setAttribute("data-suit", randomPip());
+  newHand.setAttribute("data-value", rng());
+  addCardElements(newHand);
+  display[0].appendChild(newHand);
+}
+
+function rng() {
+  let number = Math.floor(Math.random() * 13) + 1;
+  let result = "";
+  switch (number) {
+    case 1:
+      result = "A";
+      break;
+    case 11:
+      result = "J";
+      break;
+    case 12:
+      result = "Q";
+      break;
+    case 13:
+      result = "K";
+      break;
+    default:
+      result = number.toString();
+  }
+
+  return result;
+}
+
+function randomPip() {
+  let number = Math.floor(Math.random() * 4) + 1;
+  console.log(number);
+  let result = "";
+  switch (number) {
+    case 1:
+      result = "heart";
+      break;
+    case 2:
+      result = "diamond";
+      break;
+    case 3:
+      result = "spade";
+      break;
+    default:
+      result = "club";
+  }
+
+  return result;
+}
 
 function addCardElements(card) {
   const value = card.dataset.value;
